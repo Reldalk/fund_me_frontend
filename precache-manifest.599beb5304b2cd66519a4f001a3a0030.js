b08360023086e589b9fbfa1d8665b0c6b8d1d0fd
Reldalk <Reldalk@gmail.com>
2018-12-13 21:52:24 -0500

    Updates

---
+++ precache-manifest.599beb5304b2cd66519a4f001a3a0030.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "db6feb300b149257611827c9bed12f25",
+    "url": "/fund_me_frontend/static/media/treeBackground.db6feb30.png"
+  },
+  {
+    "revision": "659eb4953768e1d8d43e0a1462f37ed2",
+    "url": "/fund_me_frontend/static/media/skyBackground.659eb495.jpg"
+  },
+  {
+    "revision": "e633c2ed77f44404f16c",
+    "url": "/fund_me_frontend/static/js/runtime~main.e633c2ed.js"
+  },
+  {
+    "revision": "9a0e9af42c9fe0ba4c5d",
+    "url": "/fund_me_frontend/static/js/main.9a0e9af4.chunk.js"
+  },
+  {
+    "revision": "36fed88b87ab678bb4b3",
+    "url": "/fund_me_frontend/static/js/1.36fed88b.chunk.js"
+  },
+  {
+    "revision": "9a0e9af42c9fe0ba4c5d",
+    "url": "/fund_me_frontend/static/css/main.a493c55e.chunk.css"
+  },
+  {
+    "revision": "38189a3e33e61aa40608187d6337d03b",
+    "url": "/fund_me_frontend/index.html"
+  }
+];
